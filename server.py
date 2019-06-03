94c742ee3a07dd23d4ddf1b8b33a3a99e5c66e2a
Carlos <carlosaam96@gmail.com>
2019-06-03 16:48:59 -0500

    Finished second part of tutorial.

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import render_template
-import connexion
+import config
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api('swagger.yml')
+# Get the application instance
+connex_app = config.connex_app
 
-@app.route('/')
+# Read the swagger.yml file to configure the endpoints
+connex_app.add_api("swagger.yml")
 
+# create a URL route in our application for "/"
+@connex_app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    connex_app.run(debug=True)
